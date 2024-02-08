042316777b1c9046af36cad1db32b92a499cf53a
LauraGarcía <93741624+LauragarciaDS@users.noreply.github.com>
2024-02-08 17:10:49 +0100

    Update stg_badges.sql

--- models/staging/stg_badges.sql
+++ models/staging/stg_badges.sql
@@ -1,6 +1,6 @@
 select 
-    id as badge_id,    --aqui renombra estas columnas 
+    id as badge_id,    
     name as badge_name, 
     date as award_timestamp, 
     user_id
-from {{ source("raw", "badges") }}   -- de donde saca la informacion
+from {{ source("raw", "badges") }}   
